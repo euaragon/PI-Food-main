2e82f5de18968d7cc781d2af818cfab38a14465d
Eugenio Aragón <97991009+euaragon@users.noreply.github.com>
2023-02-23 20:22:23 -0300

    ajustes varios

--- api/src/routes/middlewares/recetaRuta.js
+++ api/src/routes/middlewares/recetaRuta.js
@@ -86,21 +86,5 @@ recipeRouter.post("/", async (req, res) => {
 
 module.exports = recipeRouter;
 
-/*
-[ ] GET /recipes?name="...":
-Obtener un listado de las recetas que contengan la palabra ingresada como query parameter
-Si no existe ninguna receta mostrar un mensaje adecuado
-
-[ ] GET /recipes/{idReceta}:
-Obtener el detalle de una receta en particular
-Debe traer solo los datos pedidos en la ruta de detalle de receta
-Incluir los tipos de dieta asociados
-
-[ ] POST /recipes:
-Recibe los datos recolectados desde el formulario controlado de la ruta de creación de recetas por body
-Crea una receta en la base de datos relacionada con sus tipos de dietas.
-
-
-*/
 
 

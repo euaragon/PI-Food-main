ed78d6ba2095822302cce437184ceaedf0dc178a
Eugenio Aragón <97991009+euaragon@users.noreply.github.com>
2023-03-17 19:02:08 -0300

    deploy

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY,
+  DB_USER, DB_PASSWORD, DB_HOST, DATABASE_URL,
 } = process.env;
 
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, {
@@ -13,7 +13,7 @@ const {
 
 
 
-const sequelize = new Sequelize(DB_DEPLOY, {
+const sequelize = new Sequelize(DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
